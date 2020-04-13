9651210a8097ecbbe9922167dd2264b289b7e483
apara <aparajitj@gmail.com>
2020-04-13 21:55:05 +0530

    Added Login and forgot password screen

--- lib/ui/MyApp.dart
+++ lib/ui/MyApp.dart
@@ -1,5 +1,9 @@
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'Forgot_password.dart';
 
 class wis extends StatefulWidget {
   @override
@@ -9,62 +13,206 @@ class wis extends StatefulWidget {
 class _wisState extends State<wis> {
 
   int _index=0;
-  List q= ["Live as if you were to die tomorrow. Learn as if you were to live forever.",
-    "That which does not kill us makes us stronger.",
-    "Be who you are and say what you feel, because those who mind don’t matter and those who matter don’t mind.",
-    "We must not allow other people’s limited perceptions to define us.",
-    "Do what you can, with what you have, where you are."
-  ];
+
+
+    bool passwordVisible = false;
+
   @override
   Widget build(BuildContext context) {
 
+    bool _obscureText = true;
+    var _emailController= new TextEditingController();
+    var _passwordController= new TextEditingController();
+    final _formKey = GlobalKey<FormState>();
+    String _password;
+    bool showLoading = false;
+    void _toggle() {
+      setState(() {
+        _obscureText = !_obscureText;
+      });
+    }
     return Scaffold(
-      backgroundColor: Colors.grey,
-        body: Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  child: Center(
-                child:Container(
-                      width:350,
-                      height:200,
-                      margin:EdgeInsets.all(30.0),
-                      decoration: BoxDecoration(
-                        color:Colors.black,
-                        borderRadius:BorderRadius.circular(20)
-
+    backgroundColor: Colors.white,
+        body:
+        Padding(
+          padding: const EdgeInsets.only(top: 60.0, left: 25.0, right: 25.0),
+          child: Theme(
+            data: ThemeData(primaryColor: Colors.amber),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 30.0),
+                    child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text("Login",
+                          style: TextStyle(
+                              fontSize: MediaQuery.of(context).size.height/20,
+                              color: Colors.grey.shade800
+                          ),
+                        )),
+                  ),
+                  TextFormField(
+                    cursorColor: Colors.black,
+                    keyboardType: TextInputType.emailAddress,
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.only(bottom: 3.0),
+                        child: Icon(Icons.email),
                       ),
-                      child: Center(child: Text(q[_index],style: TextStyle(color: Colors.red.shade900,fontStyle: FontStyle.italic, fontSize: 20,)))),
-                )),
-                Divider(thickness: 2.3,),
-                Padding(
-                  padding: const EdgeInsets.only(top:18.0),
-                  child: FlatButton.icon(onPressed: _displayQuote, icon: Icon(Icons.wb_sunny),
-                      label: Text("Next quote",
-                      style:TextStyle(
-                        color:Colors.black,
-                        fontSize: 30,
-                      )),
-                  color: Colors.red,),
+                      hintText: "Email",
+
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.amber),
+                      ),
+                    ),
+                   /* validator: (val) => !EmailValidator.validate(val, true)
+                        ? 'Not a valid email.'
+                        : null,*/
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: Stack(
+                      children: <Widget>[
+                        TextFormField(
+                          obscureText: _obscureText,
+                          cursorColor: Colors.black,
+                          keyboardType: TextInputType.text,
+                          decoration: InputDecoration(
+                            prefixIcon: Padding(
+                              padding: const EdgeInsets.only(bottom: 3.0),
+                              child: Icon(Icons.lock),
+                            ),
+                            hintText: "Password",
+
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.orange),
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value.length == 0) {
+                              return 'Password is compulsary!';
+                            } else if (value.length < 6) {
+                              return 'Password must be more than 6 charecters';
+                            }
+                            return null;
+                          },
+                          controller: _passwordController,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10.0),
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: InkWell(
+                             // onTap: toggle,
+                              child: Icon(Icons.remove_red_eye,
+                                color: Colors.grey.shade500,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        InkWell(
+                          child: Text("Forgot password?",
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                          onTap: (){
+                           Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => ForgotPassword()),
+                            );
+                          },
+                        ),
+                        InkWell(
+                          onTap: (){
+                            /*Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(builder: (context) => CreateProfile()),
+                            );*/
+                          },
+                          child: Text("New user? Sign up!",
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+       bottomNavigationBar: (
+          InkWell(
+            onTap: (){
+              var _formKey;
+              if(_formKey.currentState.validate()){
+                setState(() {
+                  showLoading = true;
+                });
+               // Login(context);
+//                if(status == 200 || status == "200"){
+//                  setState(() {
+//                    showLoading = false;
+//                  });
+//                }
+              }
+            },
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(20, 35, 20, 20),
+              child: Card(
+                child: Container(
+                  height: 55,
+                  //width: MediaQuery.of(context).size.width / 1.2,
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Colors.orange,
+                          Colors.orange,
+                        ],
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(5))),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 30.0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.white,
+                          //fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          letterSpacing: 1.0,
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
-                Spacer()
-              ],
-            )
-        )
+              ),
+            ),
+          )
+
+          )
+
+
+
     );
   }
 
-  void _displayQuote() {
-    setState(() {
-      if (_index!=4)
-      _index++;
-      else
-        _index=0;
 
-    });
-
-  }
 }
 
 class Bizcard extends StatelessWidget {
